a5cf70bc3ecf75ccdb0b61bded0076e3aeb4d2cc
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-01-28 17:56:24 -0500

    Actualización de Filtrar

--- 28012020/js/fechas.js
+++ 28012020/js/fechas.js
@@ -8,17 +8,21 @@ var diferencia = null
 function calcularDiferenciasEnDias(today, birthday, operacion) {
     if (operacion === "anos"){
         diferencia = Math.abs(today - birthday)
-        return Math.floor(diferencia / 1000 / 60 / 60 / 24 / 365)   
+        diferencia = Math.floor(diferencia / 1000 / 60 / 60 / 24 / 365)   
     }
     else if (operacion ==="dias"){
         diferencia = Math.abs(today - birthday)
-        return Math.floor(diferencia / 1000 / 60 / 60 / 24)        
+        diferencia = Math.floor(diferencia / 1000 / 60 / 60 / 24)        
     }
     else if (operacion ==="horas"){
         diferencia = Math.abs(today - birthday)
-        return Math.floor(diferencia / 1000 / 60)     
+        diferencia = Math.floor(diferencia / 1000 / 60)     
 
     }
     console.log(`La diferencia es de: ${diferencia} ${operacion}`)
 }
 
+
+document.querySelector('#calcular').addEventListener('click', function (event) {
+    calcularDiferenciasEnDias(today, birthday, operacion)
+})
