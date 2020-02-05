00aef9eb117053a975ad1c3680294d45cac8fe28
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-02-05 09:34:06 -0500

    Actualizacion de proyecto

--- CarteleraDeCine/js/funciones.js
+++ CarteleraDeCine/js/funciones.js
@@ -1,43 +1,65 @@
-console.log("el usuario no está logueado")
-
-var contenedor = document.querySelector('#contenedor_cartelera')
-var logueado = false
+console.log("Arrancó la función")
+var proximos_cont = document.querySelector('#proximosEstrenos')
+var cartelera_cont = document.querySelector('#cartelera')
 var boton_enviar = document.querySelector('.btn_enviar button')
-var url = "https://api.themoviedb.org/3/movie/now_playing?api_key="
+var url = "https://api.themoviedb.org/3/movie/"
+var urlImagen = "https://image.tmdb.org/t/p/w500"
+var apiProximos = "popular"
+var apiCartelera = "now_playing"
 var apiKey = "93aa9d7f884d7b7192389c7eb1e714a5"
-//var imagen = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
-var idGenero = {}
 var contenido = {}
+var idPelicula = ''
+var contador = 0
 
-
-function buscar() {
-    jQuery.ajax(url + apiKey + "&language=es-US&page=1", {
+function proximosEstrenos() {
+    jQuery.ajax(url + apiProximos + "?api_key=" + apiKey + "&language=es-US&page=1", {
         success: function (response) {
 
-            console.log(response)
-            contenedor.innerHTML = ""
-
-    
-            if (i = 0, i < 5, i++){
-                contenido = response[i]
-                console.log(contenido)
-        
-            }
-
-
-    response.results.forEach(function (peliculas) {
-
-        contenedor.innerHTML = contenedor.innerHTML + `            <div class="pelicula">
-                <div class="imagen"><img src="https://image.tmdb.org/t/p/w500${peliculas.backdrop_path}"></div>
-                <div class="nombre">${peliculas.title}</div>
-                <div class="sinopsis">${peliculas.overview}</div>
-                <div class="fecha">${peliculas.release_date}</div>
-            </div>`
-    })
-},
-error: function(error) {
-    console.error('No se encontraron registros, repita la búsqueda ', error)
-    contenedor.innerHTML = `<div class="error">No se encontraron registros, repita la búsqueda.</div>`
-}
+            //console.log(response)
+            proximos_cont.innerHTML = ""
+            contador = 0
+            response.results.forEach(function (peliculas) {
+                contador++
+                if (contador < 5) {
+                    proximos_cont.innerHTML = proximos_cont.innerHTML + `<div class="pelicula">
+                    <div class="imagen"><img src="${urlImagen}${peliculas.poster_path}"></div>
+                    <div class="nombre_estreno">${peliculas.title}</div>
+                    </div>`
+                }
+            })
+        },
+        error: function (error) {
+            console.error('No se encontraron registros, repita la búsqueda ', error)
+            contenedor.innerHTML = `<div class="error">No se encontraron registros, repita la búsqueda.</div>`
+        }
     })
 }
+
+function cartelera() {
+    jQuery.ajax(url + apiCartelera + "?api_key=" + apiKey + "&language=es-US&page=1", {
+        success: function (responseCartelera) {
+
+            //console.log(response)
+            cartelera_cont.innerHTML = ""
+            contador = 0
+            responseCartelera.results.forEach(function (peliculasCartelera) {
+                contador++
+                if (contador < 9) {
+                    cartelera_cont.innerHTML = cartelera_cont.innerHTML + `<div class="pelicula">
+                    <div class="imagen"><img src="${urlImagen}${peliculasCartelera.backdrop_path}"></div>
+                    <div class="nombre">${peliculasCartelera.title}</div>
+                    <div class="sinopsis">${peliculasCartelera.overview}</div>
+                    </div>`
+                }
+            })
+        },
+        error: function (error) {
+            console.error('No se encontraron registros, repita la búsqueda ', error)
+            contenedor.innerHTML = `<div class="error">No se encontraron registros, repita la búsqueda.</div>`
+        }
+    })
+}
+
+document.querySelector('.btn_enviar button').addEventListener('click', function (event) {
+    alert('Se presionó enviar')
+})
